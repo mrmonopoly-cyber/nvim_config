7fdd556c4f228534cf2a46190692ff3557e9bbe4
Alberto Damo <alberto.damo@gmail.com>
2023-10-09 23:03:55 +0200

    added parser for fsharp

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,9 +1,10 @@
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c","cpp", "lua", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -12,4 +13,15 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
   },
+}
+
+parser_config.fsharp = {
+  install_info = {
+    url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
+    branch = "develop",
+    files = {"src/scanner.cc", "src/parser.c" },
+    generate_requires_npm = true,
+    requires_generate_from_grammar = true
+  },
+  filetype = "fsharp",
 }
