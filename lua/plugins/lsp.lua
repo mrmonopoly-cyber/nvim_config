020cdafb4cc68fa452f301285b8a02a7004e7d3a
Alberto Damo <alberto.damo@gmail.com>
2025-01-29 22:42:40 +0100

    added friendly snippets and luasnip

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,6 +18,14 @@ return{
             -- Snippet plugins
             {"L3MON4D3/LuaSnip"},               -- Snippet engine
             {"saadparwaiz1/cmp_luasnip"},       -- Snippet completions
+            {
+              "L3MON4D3/LuaSnip",
+              -- follow latest release.
+              version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+              -- install jsregexp (optional!).
+              build = "make install_jsregexp",
+              dependencies = { "rafamadriz/friendly-snippets" },
+            }
         },
     },
     -- Additional Plugins (if needed)
