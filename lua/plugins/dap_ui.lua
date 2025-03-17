13dedac56b75f19623acaa9833a42a2be25aa1b6
Alberto Damo <alberto.damo@gmail.com>
2025-03-17 12:24:54 +0100

    added diffview

--- lua/plugins/dap_ui.lua
+++ lua/plugins/dap_ui.lua
@@ -1,1 +1,4 @@
-return { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+return {
+  "rcarriga/nvim-dap-ui",
+  dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+}
