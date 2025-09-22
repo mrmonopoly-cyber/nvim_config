4c6a5233fbb45828dd8a79fe0932da7d70077a88
Alberto Damo <alberto.damo@gmail.com>
2025-09-22 23:33:49 +0200

    cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -76,18 +76,8 @@ cmp.setup({
 
 lsp.setup()
 
--- arduino
-require'lspconfig'.arduino_language_server.setup{}
-
 require'cmp'.setup {
   sources = {
     { name = 'nvim_lsp_signature_help' }
-  }
-}
-
-require'lspconfig'.rust_analyzer.setup{
-  settings = {
-    ['rust-analyzer'] = {
-    }
   }
 }
