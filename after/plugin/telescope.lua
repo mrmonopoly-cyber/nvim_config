b2575b36ceb870b55267b199431028c947d1e949
Alberto Damo <alberto.damo@gmail.com>
2025-11-28 09:13:50 +0100

    cleaning of old stuff and adding make keyword <leader>mm

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {}, { desc = "find files of git repo" })
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({search = vim.fn.input("Smart Grep >")});
-end)
+end, { desc = "Smart grep" })
+vim.keymap.set("n", "<leader>fa", function()
+  require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
+end, { desc = "Find ALL files (include ignored)" })
+
